7a7a928c3da45b26618a075434fd70abf044a6c9
Ramon-24 <ramongomesbs24@gmail.com>
2024-11-15 15:01:24 -0300

    Servidor rodando

--- Back-end/src/index.js
+++ Back-end/src/index.js
@@ -1,1 +1,8 @@
-console.log("vai dar certo rodar esse servidor.")
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.listen(3000, (req, res) => {
+    console.log("O servidor Ep Boblioteca está finalmente rodando sem erros!!!!")
+});
